8401cf0c93b26287102b83b6fd01c43b7ca1dff6
Wayne Ng <wei.yang.ng@accenture.com>
2025-08-20 19:27:31 +1000

    fixed insert statements

--- setup.py
+++ setup.py
@@ -13,15 +13,15 @@ def create_database():
         )
     ''')
     cursor.execute(
-        "INSERT INTO blockout_settings (setting_name, setting_start_hour, setting_end_hour) VALUES (?, ?, ?)",
+        "INSERT INTO blockout_settings (setting_name, setting_start_hour, setting_end_hour, active) VALUES (?, ?, ?, ?)",
             ("disabled", 00, 00, 1) # Disabled setting
     )
     cursor.execute(
-        "INSERT INTO blockout_settings (setting_name, setting_start_hour, setting_end_hour) VALUES (?, ?, ?)",
+        "INSERT INTO blockout_settings (setting_name, setting_start_hour, setting_end_hour, active) VALUES (?, ?, ?)",
             ("peak", 16, 21, 0) # peak blockout setting
     )
     cursor.execute(
-        "INSERT INTO blockout_settings (setting_name, setting_start_hour, setting_end_hour) VALUES (?, ?, ?)",
+        "INSERT INTO blockout_settings (setting_name, setting_start_hour, setting_end_hour, active) VALUES (?, ?, ?)",
             ("overnight", 22, 10, 0) # overnight blockout setting
     )
     conn.commit()
