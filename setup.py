eea3923516fbe54e9d84229ec85eed2ffde98796
Wayne Ng <wei.yang.ng@accenture.com>
2025-08-20 13:15:14 +1000

    insert new default blockout setting into table

--- setup.py
+++ setup.py
@@ -7,9 +7,14 @@ def create_database():
         CREATE TABLE IF NOT EXISTS blockout_settings (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             setting_name TEXT NOT NULL,
-            setting_value TEXT NOT NULL
+            setting_start_hour INT NOT NULL,
+            setting_end_hour INT NOT NULL
         )
     ''')
+    cursor.execute(
+        "INSERT INTO blockout_settings (setting_name, setting_start_hour, setting_end_hour) VALUES (?, ?, ?)",
+            ("default_setting", 00, 00) # Default setting
+    )
     conn.commit()
     conn.close()
 
