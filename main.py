96c9e0828aa2df430b62b5f535ee7b4019c7b4a2
Wayne Ng <wei.yang.ng@accenture.com>
2025-08-20 11:49:31 +1000

    entering blockout. re-factored code

--- main.py
+++ main.py
@@ -5,18 +5,39 @@ BUTTON_GPIO_UP = 22
 BUTTON_GPIO_DOWN = 27
 BUTTON_GPIO_ENTER = 22
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUTTON_GPIO_UP, GPIO.OUT)
-GPIO.setup(BUTTON_GPIO_DOWN, GPIO.OUT)
+BUTTON_LONG_PRESS_DURATION = 3.0  # 3000ms
+BUTTON_SHORT_PRESS_DURATION = 0.2  # 200ms
 
-# Default = buttons not pressed
-GPIO.output(BUTTON_GPIO_UP, GPIO.LOW)
-GPIO.output(BUTTON_GPIO_DOWN, GPIO.LOW)
+def setup_gpio():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(BUTTON_GPIO_UP, GPIO.OUT)
+    GPIO.setup(BUTTON_GPIO_DOWN, GPIO.OUT)
+    GPIO.setup(BUTTON_GPIO_ENTER, GPIO.OUT)
+    # Default = buttons not pressed
+    GPIO.output(BUTTON_GPIO_UP, GPIO.LOW)
+    GPIO.output(BUTTON_GPIO_DOWN, GPIO.LOW)
+    GPIO.output(BUTTON_GPIO_ENTER, GPIO.LOW)
 
-# Simulate a press
-GPIO.output(BUTTON_GPIO_UP, GPIO.HIGH)
-GPIO.output(BUTTON_GPIO_DOWN, GPIO.HIGH)
-time.sleep(3)  # hold for 200ms
-GPIO.output(BUTTON_GPIO_UP, GPIO.LOW)
-GPIO.output(BUTTON_GPIO_DOWN, GPIO.LOW)
-GPIO.cleanup()
+def short_press(pin):
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(BUTTON_SHORT_PRESS_DURATION)
+    GPIO.output(pin, GPIO.LOW)
+
+def simulate_setup_mode():
+    # Simulate a long press to enter setup mode
+    GPIO.output(BUTTON_GPIO_UP, GPIO.HIGH)
+    GPIO.output(BUTTON_GPIO_DOWN, GPIO.HIGH)
+    time.sleep(BUTTON_LONG_PRESS_DURATION)
+    GPIO.output(BUTTON_GPIO_UP, GPIO.LOW)
+    GPIO.output(BUTTON_GPIO_DOWN, GPIO.LOW)
+
+def enter_blockout_mode():
+    # Simulate a short press to enter blockout mode
+    short_press(BUTTON_GPIO_DOWN)
+    short_press(BUTTON_GPIO_DOWN)
+
+if __name__ == "__main__":
+    setup_gpio()
+    simulate_setup_mode()
+    enter_blockout_mode()
+    GPIO.cleanup()
